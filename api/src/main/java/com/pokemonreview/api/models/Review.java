3a9ad68c9af843fa24943a79556e05592dc5ee27
20dcooney <davidjcooney33@gmail.com>
2024-02-14 17:02:08 -0500

    Practice with Spring Boot API creation and Modeling

---
+++ api/src/main/java/com/pokemonreview/api/models/Review.java
@@ -0,0 +1,53 @@
+package com.pokemonreview.api.models;
+
+public class Review {
+    private int id;
+    private String title;
+    private String content;
+    private int start;
+
+    //Constructors
+    public Review() {
+    }
+
+    public Review(int id, String title, String content, int start) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.start = start;
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+}
