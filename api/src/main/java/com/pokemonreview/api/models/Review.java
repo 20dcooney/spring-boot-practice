c258f0b9ebbe3dc889b547107dcccc8cb64dabd9
20dcooney <davidjcooney33@gmail.com>
2024-02-15 15:27:29 -0500

    connection to PostgreSQL setup

--- api/src/main/java/com/pokemonreview/api/models/Review.java
+++ api/src/main/java/com/pokemonreview/api/models/Review.java
@@ -1,6 +1,14 @@
 package com.pokemonreview.api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String content;
