d0716e18da8857f355593d99b44472e9e08c0c09
20dcooney <davidjcooney33@gmail.com>
2024-02-15 22:11:29 -0500

    Service Interface and Implementation, JPAImplementation, Connection to PostgreSQL success

---
+++ api/src/main/java/com/pokemonreview/api/dto/PokemonDto.java
@@ -0,0 +1,42 @@
+package com.pokemonreview.api.dto;
+
+public class PokemonDto {
+
+    private int id;
+    private String name;
+    private String type;
+
+    public PokemonDto() {
+    }
+
+    public PokemonDto(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ api/src/main/java/com/pokemonreview/api/services/PokemonService.java
@@ -0,0 +1,12 @@
+package com.pokemonreview.api.services;
+
+import com.pokemonreview.api.dto.PokemonDto;
+
+public interface PokemonService {
+
+    PokemonDto createPokemon(PokemonDto pokemonDto);
+
+
+
+
+}
